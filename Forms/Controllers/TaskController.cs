8c060674e01ec67b3010f293992526aa8888346f
Tee Link <tesuanseylink@gmail.com>
2016-04-06 20:36:25 -0500

    task index

---
+++ Forms/Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forms.Models;
+
+namespace Forms.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public ActionResult Index()
+        {
+            var tasks = new List<Task>();
+            {
+                tasks.Add(new Task() { Name = "Nick Nemeth", Description = "Initiate Forms", IsDone = false});
+                tasks.Add(new Task() { Name = "April Lee", Description = "Update Database", IsDone = true});
+                tasks.Add(new Task() { Name = "Thaddeus Bullard", Description = "Add Help File", IsDone = true});
+                tasks.Add(new Task() { Name = "Mercedes Kaestner-Varnado", Description = "Site Mantenance", IsDone = false});
+                tasks.Add(new Task() { Name = "Christopher Lindsey", Description = "End Heartache", IsDone = true});
+                tasks.Add(new Task() { Name = "Candice Dawson", Description = "Refactor Index", IsDone = false});
+                tasks.Add(new Task() { Name = "Fred Yehi", Description = "Update Database", IsDone = true});
+                tasks.Add(new Task() { Name = "Bruno Laurer", Description = "Westman API", IsDone = true});
+            }
+            return View(tasks);
+        }
+    }
+}
---
+++ Forms/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Forms.Controllers;
+
+namespace Forms.Models
+{
+    public class Task
+    {
+        public Task(string name, string description, bool isDone)
+        {
+            Name = name;
+            Description = description;
+            IsDone = isDone;
+        }
+
+        public Task()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
